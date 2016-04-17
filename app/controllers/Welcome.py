9e9c397889cf9081918fc8a78647ba1de7a828bd
Barclay Iversen <barclayiversen@gmail.com>
2016-04-16 20:05:28 -0700

    test

---
+++ app/controllers/Welcome.py
@@ -0,0 +1,11 @@
+
+from system.core.controller import *
+
+class Welcome(Controller):
+    def __init__(self, action):
+        super(Welcome, self).__init__(action)
+
+    def index(self):
+        return self.load_view('index.html')
+
+    
